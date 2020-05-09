e031baf3fe42300a741b7c3a4af8172ef40d0768
WalkerYm <869676090@qq.com>
2020-05-09 23:15:34 +0800

    初始化

---
+++ walker/scripts/main.js
@@ -0,0 +1,29 @@
+let myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hellow world!';
+
+let myImage = document.querySelector('img');
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/logo.png') {
+        myImage.setAttribute('src', 'images/logo-2.jpg');
+    } else {
+        myImage.setAttribute('src', 'images/logo.png');
+    }
+}
+
+let myButton = document.querySelector('button');
+
+function setUserName() {
+    if (!localStorage.getItem('name')) {
+        setUserName();
+    } else {
+        let storedName = localStorage.getItem('name');
+        myHeading.textContent = '一起 Hellow World!，' + storedName;
+    }
+}
+
+
+
+myButton.onclick = function() {
+    setUserName();
+}
